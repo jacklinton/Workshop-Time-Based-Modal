0ee9d27d444dc7d953ee7973e04f58184fe4a174
Jack Linton <jacklinton@mac.com>
2017-01-13 14:54:36 -0500

    Finished the consturction and styles of the navigation bar for the time being. Also added javascript functionality to change the colors and backgrounds of the buttons in the Nav bar on mouse over and out. Still need to add back in link functionality.

---
+++ NavBarBranch/scriptsNav.js
@@ -0,0 +1,31 @@
+document.getElementById("navLink").addEventListener("mouseover", function(){
+	document.getElementById("navLink").setAttribute("style", "color: green; background-color: black;")
+})
+
+document.getElementById("navLink2").addEventListener("mouseover", function(){
+	document.getElementById("navLink2").setAttribute("style", "color: green; background-color: black;")
+})
+
+document.getElementById("navLink3").addEventListener("mouseover", function(){
+	document.getElementById("navLink3").setAttribute("style", "color: green; background-color: black;")
+})
+
+document.getElementById("navLink4").addEventListener("mouseover", function(){
+	document.getElementById("navLink4").setAttribute("style", "color: green; background-color: black;")
+})
+
+document.getElementById("navLink").addEventListener("mouseout", function(){
+	document.getElementById("navLink").setAttribute("style", "color: black; background-color: lightgrey;")
+})
+
+document.getElementById("navLink2").addEventListener("mouseout", function(){
+	document.getElementById("navLink2").setAttribute("style", "color: black; background-color: lightgrey;")
+})
+
+document.getElementById("navLink3").addEventListener("mouseout", function(){
+	document.getElementById("navLink3").setAttribute("style", "color: black; background-color: lightgrey;")
+})
+
+document.getElementById("navLink4").addEventListener("mouseout", function(){
+	document.getElementById("navLink4").setAttribute("style", "color: bkacl; background-color: orange;")
+})
