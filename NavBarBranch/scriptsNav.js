ca3f4bd14757ebbdaf27f6142f8a90b39ef90369
Jack Linton <jacklinton@mac.com>
2017-01-13 15:20:55 -0500

    Added link functionality back into nav bar buttons with javascript. Need to clean up javascript cide.

--- NavBarBranch/scriptsNav.js
+++ NavBarBranch/scriptsNav.js
@@ -1,17 +1,21 @@
 document.getElementById("navLink").addEventListener("mouseover", function(){
 	document.getElementById("navLink").setAttribute("style", "color: green; background-color: black;")
+	document.getElementById("homeLink").setAttribute("href", "https://www.google.com")
 })
 
 document.getElementById("navLink2").addEventListener("mouseover", function(){
 	document.getElementById("navLink2").setAttribute("style", "color: green; background-color: black;")
+	document.getElementById("naLink").setAttribute("href", "https://www.google.com")
 })
 
 document.getElementById("navLink3").addEventListener("mouseover", function(){
 	document.getElementById("navLink3").setAttribute("style", "color: green; background-color: black;")
+	document.getElementById("contactLink").setAttribute("href", "https://www.google.com")
 })
 
 document.getElementById("navLink4").addEventListener("mouseover", function(){
 	document.getElementById("navLink4").setAttribute("style", "color: green; background-color: black;")
+	document.getElementById("forumLink").setAttribute("href", "https://www.google.com")
 })
 
 document.getElementById("navLink").addEventListener("mouseout", function(){
